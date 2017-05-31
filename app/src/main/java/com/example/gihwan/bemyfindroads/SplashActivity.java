0e7807ddf3ee00a0d44a93a10fc6e1fe19b5a3d1
GiHwanHong <tdr1008@naver.com>
2017-05-31 14:12:24 +0900

    First commit (google Api ,Splash Add, STT, Source Code modity)

--- app/src/main/java/com/example/gihwan/bemyfindroads/SplashActivity.java
+++ app/src/main/java/com/example/gihwan/bemyfindroads/SplashActivity.java
@@ -4,24 +4,56 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static java.lang.Boolean.TRUE;
 
 
 /**
  * Created by GiHwan on 2017. 5. 3..
  */
 
+public class SplashActivity extends Activity{
+    private Timer timer;
+    private ProgressBar progressBar;
+    private int i=0;
+    TextView textView;
+    final long period = 10;
 
-public class SplashActivity extends Activity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Handler handler=new Handler();
-        handler.postDelayed(new Runnable() {
+        progressBar=(ProgressBar)findViewById(R.id.progressBar);
+        progressBar.setProgress(0);
+        textView=(TextView)findViewById(R.id.textView);
+        textView.setText("");
+
+        timer=new Timer();
+        timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                finish();
+                //this repeats every 100 ms
+                 if(i<100){
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            textView.setText(String.valueOf(i)+"%");
+                        }
+                    });
+                    progressBar.setProgress(i);
+                    i++;
+                }
+                if(i==100) {
+                    timer.cancel();
+                    finish();
+                }
             }
-        },3000);
+        }, 0, period);
     }
 }
